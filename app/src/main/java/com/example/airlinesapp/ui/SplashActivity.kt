0568f79519ebe14690014fcfcb97927d69f7bdff
ayashawahneh <ayashawahneh@harri.com>
2021-11-24 10:00:19 +0200

    homeScreen: create navigation

--- app/src/main/java/com/example/airlinesapp/ui/SplashActivity.kt
+++ app/src/main/java/com/example/airlinesapp/ui/SplashActivity.kt
@@ -17,7 +17,7 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
             finish()
         }, SPLASHING_TIME.toLong())
