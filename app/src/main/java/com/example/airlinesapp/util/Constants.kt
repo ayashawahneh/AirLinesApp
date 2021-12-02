7da9382165bcae14f903c7ec8f4a5a6afcb68418
ayashawahneh <ayashawahneh@harri.com>
2021-12-02 03:59:34 +0200

    1-add the floating Button 2-edit theme 3-do some changes on files formatting and packaging 4-add progressBar to the passengersList (stick on it did not work correctly)

--- app/src/main/java/com/example/airlinesapp/util/Constants.kt
+++ app/src/main/java/com/example/airlinesapp/util/Constants.kt
@@ -1,28 +1,18 @@
-package com.example.airlinesapp.di.modules
+package com.example.airlinesapp.util
 
-import com.example.airlinesapp.ui.home.airlines.AirlinesFragment
-import com.example.airlinesapp.ui.home.airlines.addAirline.AddAirlineActivity
-import com.example.airlinesapp.ui.home.passengers.PassengersFragment
-import com.example.airlinesapp.ui.home.passengers.addPassenger.AddPassengerActivity
-import com.example.airlinesapp.ui.home.passengers.editPassenger.EditPassengerActivity
-import dagger.Module
-import dagger.android.ContributesAndroidInjector
+object Constants {
 
-@Module
-abstract class BuildersModule {
+    const val SPLASHING_TIME = 2000
+    const val PASSENGERS_PER_PAGE = 10
+    const val FIRST_PAGE = 0
 
-    @ContributesAndroidInjector
-    abstract fun contributeAirlinesFragment(): AirlinesFragment
+    const val LOADING = "Loading"
+    const val CHECK_NETWORK_ERROR = "Check your network connection."
+    const val EMPTY_LIST = "No data to view"
 
-    @ContributesAndroidInjector
-    abstract fun contributePassengersFragment(): PassengersFragment
+    const val FIRST_AIRLINE_YEAR = 1919
+    const val AIRLINE_RESULT_CODE = 123
+    const val PASSENGER_RESULT_CODE = 321
+    const val FAVORITE_AIRLINE_DATASTORE ="datastore"
 
-    @ContributesAndroidInjector
-    abstract fun contributeAddAirlineActivity(): AddAirlineActivity
-
-    @ContributesAndroidInjector
-    abstract fun contributeAddPassengerActivity(): AddPassengerActivity
-
-    @ContributesAndroidInjector
-    abstract fun contributeEditPassengerActivity(): EditPassengerActivity
 }
